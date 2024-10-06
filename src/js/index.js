42b69ec6dc94ea60ef8b149888b32499f8b10cc2
Evgeny <dirath2014@gmail.com>
2024-10-06 18:16:37 +0300

    уберёт пару лишних дефолтных скриптов из сборки

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,14 @@
 import 'wicg-inert';
 import 'what-input';
-import { testSupportWebp } from './modules/test-support-webp.js';
 import { defocusButton } from './modules/defocus-button.js';
-import { initLinkScroll } from './modules/init-link-scroll.js';
 import { initPhoneMask } from './modules/init-phone-mask.js';
 import { initPhoneHref } from './modules/init-phone-href.js';
 import { initMenu } from './modules/init-menu.js';
 import { initSwiper } from './modules/init-swiper.js';
 import { initQuestionsDropdown } from './modules/questions-drop-down.js';
 
-testSupportWebp();
 defocusButton();
 initPhoneHref();
-initLinkScroll();
 initPhoneMask();
 initMenu();
 initSwiper();
